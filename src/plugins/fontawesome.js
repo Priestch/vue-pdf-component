00f28c1b19a522f2e150da0b598fe82918b4846f
Priestch <priestch@gmail.com>
2018-12-12 22:19:39 +0800

    import used fontawesome icons on demand

--- src/plugins/fontawesome.js
+++ src/plugins/fontawesome.js
@@ -1,9 +1,9 @@
 import Vue from 'vue';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons/faSpinner';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-library.add(fas);
+library.add(faSpinner);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
